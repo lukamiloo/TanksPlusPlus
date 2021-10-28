8a884a4a514967712a631624185e2918e65f51d8
Jimmy Zhang <jimmy.zhang@queensu.ca>
2021-10-27 23:31:24 -0400

    Create Player header and source file

--- tanks++/Game.cpp
+++ tanks++/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "Player.h"
 
 // constructor
 Game::Game() {
---
+++ tanks++/Player.h
@@ -0,0 +1,21 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Audio.hpp>
+#include <iostream>
+
+/*
+ *	Player
+ *
+ *	Player class. Movement, controls, and textures will be handled here
+ */
+class Player {
+private:
+public:
+	Player();
+};
+
+#endif
