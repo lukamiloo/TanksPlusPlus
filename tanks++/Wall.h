b20498fffc93211bf976626d657843eeac19a535
Jimmy Zhang <jimmy.zhang@queensu.ca>
2021-10-27 23:52:19 -0400

    Create Wall header and source file

---
+++ tanks++/Wall.h
@@ -0,0 +1,17 @@
+#ifndef WALL_H
+#define WALL_H
+
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Audio.hpp>
+#include <iostream>
+
+class Wall {
+private:
+public:
+	Wall();
+	virtual ~Wall();
+};
+
+#endif
