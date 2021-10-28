7b4823629104367a826fae4d95f546cdeaebeee3
Jimmy Zhang <jimmy.zhang@queensu.ca>
2021-10-27 22:15:01 -0400

    Create Game header and source file

---
+++ tanks++/Game.cpp
@@ -0,0 +1,8 @@
+#include "Game.h"
+
+/*
+	Main game class.
+*/
+Game::Game() {
+
+}
---
+++ tanks++/Game.h
@@ -0,0 +1,16 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Audio.hpp>
+#include <iostream>
+
+class Game {
+private:
+public:
+	Game();
+};
+
+#endif
